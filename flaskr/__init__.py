528505505a9ebc3d7069748b0d0d281224819a32
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-03-20 10:08:09 -0500

    auth.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,13 +2,21 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    from . import db
+    db.init_app(app)
+
+    return app
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+
+        )
+    
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
